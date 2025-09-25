bac5e5457a30c50c0c4dc6334f69d14aafb6b3dc
Никита <nik-ta@mail.ru>
2025-09-25 14:43:44 +0300

    add js code open close window form

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const choice = document.querySelector('.choice');
 const closeNav = document.querySelector('.header--nav-bar-close');
 const openNav = document.querySelector('.header--nav-open');
 const headerNavBarLink = document.querySelector('.header--nav-bar__link');
-const choiceBtn = document.querySelector('.choice--link-btn')
-
+const choiceBtn = document.querySelector('.choice--link-btn');
+const choiceBtnClose = document.querySelector('.choice--btn-close');
 // Включает выключает тему
 themeToggle.addEventListener('change', () => {
   body.classList.toggle('dark');
   choice.classList.toggle('dark');
-  openNav.classList.toggle('white')
-  closeNav.classList.toggle('white')
+  openNav.classList.toggle('white');
+  closeNav.classList.toggle('white');
   localStorage.setItem(
     'theme',
     body.classList.contains('dark') ? 'dark' : 'light'
@@ -21,26 +21,29 @@ themeToggle.addEventListener('change', () => {
 if (localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark');
   choice.classList.add('dark');
-  openNav.classList.add("white")
-  closeNav.classList.add("white")
+  openNav.classList.add('white');
+  closeNav.classList.add('white');
   themeToggle.checked = true;
 }
 
-
 // Скрывает и открывает меню
 openNav.addEventListener('click', () => {
-  document.querySelector(".header--nav-bar").classList.remove("close")
+  document.querySelector('.header--nav-bar').classList.remove('close');
   document.querySelector('.header--nav-bar').classList.add('open');
-  
 });
 
-closeNav.addEventListener("click", ()=>{
-  document.querySelector(".header--nav-bar").classList.remove("open")
-  document.querySelector(".header--nav-bar").classList.add('close')
-  
-})
+closeNav.addEventListener('click', () => {
+  document.querySelector('.header--nav-bar').classList.remove('open');
+  document.querySelector('.header--nav-bar').classList.add('close');
+});
 
 // При нажатии на связь что бы выходила окно форм для ввода номера и имени
-choiceBtn.addEventListener("click", (e)=>{
-  
+choiceBtn.addEventListener('click', (e) => {
+  document.querySelector('.choice--challenge').classList.add('block');
+  body.classList.add('block');
+});
+
+choiceBtnClose.addEventListener("click", ()=>{
+  document.querySelector('.choice--challenge').classList.remove('block');
+  body.classList.remove('block');
 })
