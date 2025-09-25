9b6c6223c303380ee411b8eaf70d9bd5ee75efea
Никита <nik-ta@mail.ru>
2025-09-25 16:29:03 +0300

    refactoring and add form js

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const openNav = document.querySelector('.header--nav-open');
 const headerNavBarLink = document.querySelector('.header--nav-bar__link');
 const choiceBtn = document.querySelector('.choice--link-btn');
 const choiceBtnClose = document.querySelector('.choice--btn-close');
+const choiceImgReliability = document.querySelector('.choice--img-reliability');
+const choiceForm = document.querySelector('.choice--form')
 // Включает выключает тему
 themeToggle.addEventListener('change', () => {
   body.classList.toggle('dark');
   choice.classList.toggle('dark');
   openNav.classList.toggle('white');
   closeNav.classList.toggle('white');
+  choiceImgReliability.classList.toggle('white');
   localStorage.setItem(
     'theme',
     body.classList.contains('dark') ? 'dark' : 'light'
@@ -23,27 +26,49 @@ if (localStorage.getItem('theme') === 'dark') {
   choice.classList.add('dark');
   openNav.classList.add('white');
   closeNav.classList.add('white');
+  choiceImgReliability.classList.toggle('white');
   themeToggle.checked = true;
 }
 
+const addClass = (docName, className) => {
+  document.querySelector(docName).classList.add(className);
+};
+
+const removeClass = (docName, className) => {
+  document.querySelector(docName).classList.remove(className);
+};
+
 // Скрывает и открывает меню
 openNav.addEventListener('click', () => {
-  document.querySelector('.header--nav-bar').classList.remove('close');
-  document.querySelector('.header--nav-bar').classList.add('open');
+  // document.querySelector('.header--nav-bar').classList.remove('close');
+  // document.querySelector('.header--nav-bar').classList.add('open');
+  removeClass('.header--nav-bar', 'close');
+  addClass('.header--nav-bar', 'open');
 });
 
 closeNav.addEventListener('click', () => {
-  document.querySelector('.header--nav-bar').classList.remove('open');
-  document.querySelector('.header--nav-bar').classList.add('close');
+  // document.querySelector('.header--nav-bar').classList.remove('open');
+  // document.querySelector('.header--nav-bar').classList.add('close');
+  removeClass('.header--nav-bar', 'open');
+  addClass('.header--nav-bar', 'close');
 });
 
 // При нажатии на связь что бы выходила окно форм для ввода номера и имени
 choiceBtn.addEventListener('click', (e) => {
-  document.querySelector('.choice--challenge').classList.add('block');
-  body.classList.add('block');
+  // document.querySelector('.choice--challenge').classList.add('block');
+  // body.classList.add('block');
+  addClass('.choice--challenge', 'block');
+  addClass('body', 'block');
 });
 
-choiceBtnClose.addEventListener("click", ()=>{
-  document.querySelector('.choice--challenge').classList.remove('block');
-  body.classList.remove('block');
+choiceBtnClose.addEventListener('click', () => {
+  // document.querySelector('.choice--challenge').classList.remove('block');
+  // body.classList.remove('block');
+  removeClass('.choice--challenge', 'block');
+  removeClass('body', 'block');
+});
+
+choiceForm.addEventListener("submit", (e)=>{
+  e.target.preventDefault()
+  console.log(135);
 })
